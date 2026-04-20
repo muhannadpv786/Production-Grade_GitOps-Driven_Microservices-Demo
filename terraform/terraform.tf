a51e612ca156a50c2edc496d75353984654c2b7e
MUHANNED PV <p.vmuhannad786@gmail.com>
2026-04-20 22:21:45 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -11,3 +11,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "devopsdock-terraform-backend-bucket"
+    key    = "s3-backend"
+    region = "ap-south-1"
+  }
+}
